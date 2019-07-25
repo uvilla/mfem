e9553dd3dc909b25e58207bc634f829818ae16ed
Yohann Dudouit <yohann.dudouit@gmail.com>
2019-07-25 16:31:28 -0700

    Make /gpu/cuda/gen the default ceed-cuda backend.

--- general/device.cpp
+++ general/device.cpp
@@ -283,7 +283,7 @@ void Device::Setup(const int device)
    {
       if (!ceed_option)
       {
-         CeedDeviceSetup("/gpu/cuda/ref");
+         CeedDeviceSetup("/gpu/cuda/gen");
       }
       else
       {
